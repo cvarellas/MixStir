6ba42b199ea159662266697829428ee20825edf9
Nancy <wenzhengkong2018@u.northwestern.edu>
2017-11-28 17:05:54 -0800

    first draft

--- js/shoppinglist.js
+++ js/shoppinglist.js
@@ -1,56 +1,69 @@
 $(document).ready(function(){
     $('#list').hide();
+    $('.dim').hide();
     
     $('.form-control').click(     
         function(event){
-            event.stopPropagation();
-            $("#list").fadeIn("fast");
-            $('.main').fadeOut(0.01);
-            $('.map').fadeOut(0.01);
-            $('.title').fadeOut(0.01);
-            $('.shlist').fadeOut(0.01);
+     event.stopPropagation();
+     $("#list").fadeIn("fast");
+     $('.main').fadeOut(0.01);
+     $('.map').fadeOut(0.01);
+     $('.title').fadeOut(0.01);
+     $('.shlist').fadeOut(0.01);
+     $('.result').fadeOut(0.01);
+   $('.announce').fadeOut(0.01);
+
         }
     );
     
     $('#close').click( function(){
-        $('#list').hide();
-        $('.main').fadeIn(0.01);
-        $('.map').fadeIn(0.01);
-        $('.title').fadeIn(0.01);
-        $('.shlist').fadeIn(0.01);
+    $('#list').hide();
+    $('.main').fadeIn(0.01);
+    $('.map').fadeIn(0.01);
+    $('.title').fadeIn(0.01);
+    $('.shlist').fadeIn(0.01);
+    $('.result').fadeIn(0.01);
+    $('.announce').fadeIn(0.01);
     });
+    
+
     
     $('.one').click(function(){
        window.location.href = "wholefoods.html" 
     });
 
+    $('.ellipsis').click(function(){
+    $('.collapsible').css({"transform": "TranslateY(0)"});
+    $('.dim').fadeIn(0.01);
+        $('.dim').css({"z-index": "0"});
+        $('.navbar').css({"z-index": "-1"});
+        $('.collapsible').css({"z-index": "1"});
+    });
     
+    $('.cancel').click(function(){
+        $('.collapsible').css({"transform": "TranslateY(100%)"});
+        $('.dim').fadeOut(0);
+    });
     
     
     $('.plus').click(function(){
-        document.getElementById('slnum').innerHTML = '(8)';
-        document.getElementById('slnum').style.color = "white";
-        document.getElementById('sumup').innerHTML = '8'; document.getElementById('sumup').style.color = "#fe7a55";
+        
 
     });
-    
-    $("#quantity").change(function(){  
-       document.getElementById('sumup').innerHTML = '7'; document.getElementById('sumup').style.color = "#fe7a55";
-       document.getElementById('totalprice').innerHTML = '$21.48-$36.63';
-    });
     $("#quantityflour").change(function(){  
-       document.getElementById('sumup').innerHTML = '8'; document.getElementById('sumup').style.color = "#fe7a55";
-       document.getElementById('totalprice').innerHTML = '$22.28-$38.03';
+       document.getElementById('sumup').innerHTML = '12'; document.getElementById('sumup').style.color = "#fe7a55";
+       document.getElementById('totalprice').innerHTML = '$42.61-$62.43';
         
+       document.getElementById('slnum').innerHTML = '(12)';
+       document.getElementById('slnum').style.color = "white";
     });
-    
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox.streets',
-    accessToken: 'your.mapbox.access.token'
-}).addTo(mymap);
+    $("#quantitytea").change(function(){  
+       document.getElementById('sumup').innerHTML = '13'; document.getElementById('sumup').style.color = "#fe7a55";
+       document.getElementById('totalprice').innerHTML = '$52.1-$72.42';
+        
+       document.getElementById('slnum').innerHTML = '(13)';
+       document.getElementById('slnum').style.color = "white";
+    });
 
 var availableTags = [
       "apple", "pumpkin", "basil", "banana", "cinnamon", "apple(recipe)", "egg", "egg tart", "french toast", "egg benedict", "muffin", "craneberry", "spinach", "turkey", "Thanksgiving dinner"
